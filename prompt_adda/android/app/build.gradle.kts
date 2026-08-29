b14d6b0c02e18d7e761788f99e33d2f6dd9a75ba
kushalkanagallakk-prog <kanagallakushalkumar5@gmail.com>
2026-08-29 19:13:54 +0530

    Configure production Android release signing

--- prompt_adda/android/app/build.gradle.kts
+++ prompt_adda/android/app/build.gradle.kts
@@ -1,3 +1,11 @@
+import java.io.FileInputStream
+import java.util.Properties
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
 plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
@@ -14,6 +22,15 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    signingConfigs {
+    create("release") {
+        keyAlias = keystoreProperties["keyAlias"] as String
+        keyPassword = keystoreProperties["keyPassword"] as String
+        storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+        storePassword = keystoreProperties["storePassword"] as String
+    }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.promptadda.app"
@@ -26,11 +43,9 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+    release {
+        signingConfig = signingConfigs.getByName("release")
+    }
     }
 }
 
